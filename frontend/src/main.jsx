e461dfee3a59d901fb4c5e46485b2f931c9b7010
bsquiroz <bemq1605@gmail.com>
2023-10-26 17:50:05 -0500

    change brouser for hash

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,12 @@ import { ChatApp } from "./ChatApp.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
+	<HashRouter>
 		<ChatApp />
 		<ToastContainer />
-	</BrowserRouter>
+	</HashRouter>
 );
